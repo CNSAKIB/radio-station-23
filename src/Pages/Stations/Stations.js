9cfe603ea0c19281e7a7827cbefec719d5d43678
CNSAKIB <c.nazmuzsakib@gmail.com>
2024-01-31 16:05:59 +0600

    two new components added named stations and navigation

---
+++ src/Pages/Stations/Stations.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+const Stations = () => {
+
+    return (
+        <div>
+            <h1>This is Stations</h1>
+            <Button variant="primary">Primary</Button>
+        </div>
+    );
+};
+
+export default Stations;
